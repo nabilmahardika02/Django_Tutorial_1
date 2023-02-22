a6d7f549eb092e52a202d3cdf401277fb2d2566e
nabilmahardika02 <mmahardikan@gmail.com>
2023-02-22 14:04:58 +0700

    first commit

--- money_tracker/urls.py
+++ money_tracker/urls.py
@@ -1,8 +1,19 @@
 from money_tracker.views import show_tracker
 from django.urls import path, include
+from money_tracker.views import create_transaction
+from money_tracker.views import show_xml 
+from money_tracker.views import show_json 
+from money_tracker.views import show_xml_by_id, show_json_by_id 
+
 
 app_name = 'money_tracker'
 
 urlpatterns = [
     path('', show_tracker, name='show_tracker'),
+    path('xml/', show_xml, name='show_xml'), 
+    path('create', create_transaction, name='create_transaction'),
+    path('xml/', show_xml, name='show_xml'), 
+    path('json/', show_json, name='show_json'), 
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
 ]
